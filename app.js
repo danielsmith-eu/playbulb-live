682740f92a71953a533ffabd32a1ba114a41c2f3
Dr Dan Smith <dan.smith@thomsonreuters.com>
2015-05-06 09:58:49 +0100

    Implemented all effects
    Code cleaned up
    Demo cycle mode added

--- app.js
+++ app.js
@@ -1,20 +1,49 @@
+'use strict';
 var Playbulb = require('./playbulb');
 
-var pb = Playbulb();
-
-pb.ready(function () {
-
-    var change = function () {
-        var r = rand(256), g = rand(256), b = rand(256);
-        pb.setColor(r, g, b);
-        setTimeout(change, 100);
-    };
-    //change();
-
-    pb.setPulse(255,0,255,1);
-});
-
-
-function rand(n) {
+function randInt(n) {
     return Math.floor(Math.random() * n);
 }
+
+var pb = new Playbulb();
+pb.ready(function () {
+    console.log("Playbulb Demo Mode");
+    var flashMax = 20;
+    var doEffects = function (effectNum) {
+        var r = randInt(256), g = randInt(256), b = randInt(256);
+        switch (effectNum) {
+            default:
+            case 0:
+                console.log("Running PULSE effect at max speed with red: " + r + ", green: " + g + ", blue: " + b);
+                pb.setPulse(r, g, b, 1.0);
+                break;
+            case 1:
+                console.log("Running FLASH effect at max speed with red: " + r + ", green: " + g + ", blue: " + b);
+                pb.setFlash(r, g, b, 1.0);
+                break;
+            case 2:
+                console.log("Running RAINBOW JUMP effect at max speed with red: " + r + ", green: " + g + ", blue: " + b);
+                pb.setRainbowJump(r, g, b, 1.0);
+                break;
+            case 3:
+                console.log("Running RAINBOW FADE effect at max speed with red: " + r + ", green: " + g + ", blue: " + b);
+                pb.setRainbowFade(r, g, b, 1.0);
+                break;
+            case 4:
+                setTimeout(function () { changeColours(flashMax); }, 2500);
+                return;
+        }
+        setTimeout(function() { doEffects(effectNum + 1); }, 5000);
+    };
+    var changeColours = function (flashCount) {
+        if (flashCount === 0) {
+            doEffects(0);
+        } else {
+            var r = randInt(256), g = randInt(256), b = randInt(256);
+            console.log("Setting colour to red: " + r + ", green: " + g + ", blue: " + b);
+            pb.setColor(r, g, b);
+            setTimeout(function() { changeColours(flashCount - 1); }, 100);
+        }
+    };
+    changeColours(flashMax);
+});
--- playbulb.js
+++ playbulb.js
@@ -1,3 +1,4 @@
+'use strict';
 var noble = require('noble');
 
 var Playbulb = function (playbulbName, serviceUuid, colorUuid, effectsUuid) {
@@ -7,93 +8,114 @@ var Playbulb = function (playbulbName, serviceUuid, colorUuid, effectsUuid) {
     effectsUuid = effectsUuid || "fffb";
 
     var colorChar, effectsChar;
-
     var yesReady = false;
     var waiting = [];
+    var modes = {
+        FLASH: 0,
+        PULSE: 1,
+        RAINBOWJUMP: 2,
+        RAINBOWFADE: 3
+    };
+
     var isReady = function (callback) {
         if (callback) {
             if (yesReady) {
-                callback();
+                setTimeout(callback, 0); // run async
             } else {
                 waiting.push(callback);
             }
-        } else if (colorChar != null && effectsChar != null) {
+        } else if (colorChar !== null && effectsChar !== null) {
             yesReady = true;
+            var waiter;
             while (waiting.length > 0) {
-                var waiter = waiting.pop(0);
-                waiter();
+                waiter = waiting.pop(0);
+                setTimeout(waiter, 0); // run each waiter async
             }
+        }
+    };
+
+    var decimalToHexBytes = function (speed, max) {
+        var speedRanged = speed * max;
+        var speedHex = speedRanged.toString(16);
+        while (speedHex.length < 4) {
+            speedHex = "0" + speedHex;
+        }
+        return [parseInt(speedHex.substring(0, 2), 16), parseInt(speedHex.substring(2, 4), 16)];
+    };
+
+    var runEffect = function (r, g, b, effect, speed) {
+        if (!isReady) {
+            throw "playbulb not ready";
+        }
+        if (r < 0 || r > 255 || g < 0 || g > 255 || b < 0 || b > 255) {
+            throw "r, g and b must be between 0 and 255";
+
+        }
+        if (effect !== null) {
+            if (speed < 0 || speed > 1) {
+                throw "speed must be between 0 and 1";
+            }
+            speed = 1 - speed; // 0 is slow, 1 is fast
+            var max = effect === modes.PULSE ? 7710 : 17990;
+            var speedBytes = decimalToHexBytes(speed, max); // max hex is: 1E 1E
+            var effectBytes = new Buffer([0, r, g, b, effect, 0, speedBytes[0], speedBytes[1]]);
+            effectsChar.write(effectBytes);
+        } else {
+            var colorBytes = new Buffer([0, r, g, b]);
+            colorChar.write(colorBytes);
         }
     };
 
     noble.startScanning();
     noble.on('discover', function (peripheral) {
-        if (peripheral.advertisement.localName == playbulbName) {
+        if (peripheral.advertisement.localName === playbulbName) {
             peripheral.connect(function (error) {
+                if (error) {
+                    throw error;
+                }
                 peripheral.discoverAllServicesAndCharacteristics();
                 peripheral.on('servicesDiscover', function (services) {
                     services.map(function (service) {
-                        if (service.uuid == serviceUuid) {
+                        if (service.uuid === serviceUuid) {
                             service.on('characteristicsDiscover', function (characteristics) {
                                 characteristics.map(function (characteristic) {
-                                    if (characteristic.uuid == colorUuid) {
+                                    if (characteristic.uuid === colorUuid) {
                                         colorChar = characteristic;
                                         isReady();
-                                    } else if (characteristic.uuid == effectsUuid) {
+                                    } else if (characteristic.uuid === effectsUuid) {
                                         effectsChar = characteristic;
                                         isReady();
                                     }
                                 });
                             });
-                        };
+                        }
                     });
                 });
             });
         }
     });
 
-    var modes = {
-        FLASH: 0,
-        PULSE: 1,
-        RAINBOWJUMP: 2,
-        RAINBOWFADE: 3,
-    };
-
-    var decimalToHexBytes = function (speed, max) {
-            var speedRanged = speed * max;
-            var speedHex = speedRanged.toString(16);
-            while(speedHex.length < 4) {
-                speedHex = "0" + speedHex;
-            }
-            return [parseInt(speedHex.substring(0, 2), 16), parseInt(speedHex.substring(2, 4), 16)];
-    };
 
     return {
         setColor: function (r, g, b) {
-            if (r < 0 || r > 255 || g < 0 || g > 255 || b < 0 || b > 255) {
-                throw new Exception("in setColor r, g and b must be between 0 and 255");
-            }
-            var color = new Buffer([0, r, g, b]);
-            if (colorChar) {
-                colorChar.write(color);
-            }
+            runEffect(r, g, b);
         },
         setPulse: function (r, g, b, speed) {
-            if (speed < 0 || speed > 1) { 
-                throw new Exception("in setPulse speed must be between 0 and 1");
-            }
-            speed = 1 - speed; // 0 is slow, 1 is fast
-            var speedBytes = decimalToHexBytes(speed, 17990); // max hex is: 46 46
-            var effect = new Buffer([0, r, g, b, modes.PULSE, 0, speedBytes[0], speedBytes[1]]);
-            console.log(effect);
-            if (effectsChar) {
-                effectsChar.write(effect);
-            } 
+            runEffect(r, g, b, modes.PULSE, speed);
+        },
+        setFlash: function (r, g, b, speed) {
+            runEffect(r, g, b, modes.FLASH, speed);
+        },
+        setRainbowJump: function (r, g, b, speed) {
+            runEffect(r, g, b, modes.RAINBOWJUMP, speed);
+        },
+        setRainbowFade: function (r, g, b, speed) {
+            runEffect(r, g, b, modes.RAINBOWFADE, speed);
         },
         ready: function (callback) {
             isReady(callback);
-        },
-    }
+        }
+    };
 };
 
 module.exports = Playbulb;
